bf0ab2b87070e488d0ee6aab33cfb66aa85d57a0
fahmiprasanda <fahmiprassanda@yahoo.com>
2014-05-14 16:09:16 +0700

    new

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/service/AppService.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/service/AppService.java
@@ -1,6 +1,7 @@
 package net.smktarunabhakti.penjualan.service;
 
 import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.Pageable;
 
 import net.smktarunabhakti.penjualan.domain.Barang;
